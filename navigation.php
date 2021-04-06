149f8a8219a6345356da5723a8ead8d38999a0fb
gagan <gagangowda841@gmail.com>
2021-04-06 09:56:06 +0530

    c

--- navigation.php
+++ navigation.php
@@ -119,7 +119,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav m-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="#">Home</a>
+          <a class="nav-link" aria-current="page" href="index.php">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" aria-current="page" href="solar.php">Solar Water Heaters</a>
--- product-details.php
+++ product-details.php
@@ -14,10 +14,131 @@
                 Supreme Solar Water Heater 200LPD Glass lined Model 
             </h2>
             <p class="mt-4 text-grey">
-                Product Description : Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum minus, quis voluptatem obcaecati est ea nesciunt qui sit temporibus!
+                Product Description : Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum minus, quis voluptatem 
+                obcaecati est ea nesciunt qui sit temporibus!
             </p>
+            <table class="table table-responsive">
+                <thead>
+                    <tr>
+                        <th colspan="2">
+                            Specifications
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>
+                            Capacity:	
+                        </th>
+                        <td>
+                            220 LPD/Liters
+
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            Circulation Type:	
+                        </th>
+                        <td>
+                            Thermosyphon
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            Coating	
+                        </th>
+                        <td>
+                            Porcelain Enamel (Glass lined)
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            Maximum Water Hardness		
+                        </th>
+                        <td>
+                            1000 ppm
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            No. Of Tubes		
+                        </th>
+                        <td>
+                            17
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            Pressure		
+                        </th>
+                        <td>
+                            1 bar
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            Product Certification		
+                        </th>
+                        <td>
+                            MNRE Approved ISO 9001:2000
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            Space Required		
+                        </th>
+                        <td>
+                            2*2.5 (W*L)
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            Tank Insulation		
+                        </th>
+                        <td>
+                            PUF- 50 mm(Polyurethane Foam)
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            Tube length		
+                        </th>
+                        <td>
+                            2100 mm
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            Tube Size		
+                        </th>
+                        <td>
+                            58 mm
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            Type :	
+                        </th>
+                        <td>
+                            Evacuated Tube Collector (ETC)
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            Warranty	
+                        </th>
+                        <td>
+                            10 Years
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
+</div>
+
+<div class="container">
+    
 </div>
 
 <div class="p-3"></div>
